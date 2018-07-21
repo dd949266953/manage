2f5c93fed6bf483630ea93371853bc844b9bb431
dandanran <949266952@qq.com>
2018-07-21 19:40:06 +0800

    住户接口

--- sj_dao/src/main/java/com/m78/mapper/TenementMapper.java
+++ sj_dao/src/main/java/com/m78/mapper/TenementMapper.java
@@ -18,5 +18,35 @@ public interface TenementMapper {
 
     int updateByPrimaryKey(Tenement record);
 
+    /**
+     * 获取所有住户
+     * @return
+     */
     List<Tenement> getAllTenement();
+
+    /**
+     * 根据住户姓名获取住户详细住址
+     * @return
+     */
+    List<Tenement> getDetailAddressByName(@Param("tenementName")String tenementName,@Param("start") int start,@Param("limit") int limit);
+
+    /**
+     * 根据住户姓名查询住户总数
+     * @return
+     */
+    int getTenementCountByName(@Param("name")String name);
+
+    /**
+     * 根据住户手机号查询车位主键
+     * @param phone
+     * @return
+     */
+    Long getCarportIdByTenementPhone(@Param("phone")String phone);
+
+    /**
+     * 根据住户手机号查询房屋主键
+     * @param phone
+     * @return
+     */
+    Long getHouseIdByTenementPhone(@Param("phone")String phone);
 }
